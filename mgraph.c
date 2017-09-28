485c96b4d03287e3eb424ac9920357be144f79fc
Guilherme Nogima <gbnogima@gmail.com>
2017-09-28 00:24:53 -0300

    adsf

--- mgraph.c
+++ mgraph.c
@@ -130,7 +130,7 @@ void create_matrix (float **M, int n){
 
 int vertice_mais_central (float **M, int n){
     int i, j;
-    float aux = malloc(sizeof(float));
+    float *aux = malloc(sizeof(float));
     for (j = 0; j < n; j++){
         int maior = M[0][j];
         for (i = 0; i < n; i++){
